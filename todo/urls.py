a2a709a21d8206065176c8a7b324db40bb61e982
Sikach Illia <sikatch.ilja@gmail.com>
2024-03-11 16:10:47 +0200

    All done

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 
 from .views import (
-            TaskListView,
-            TaskCreateView,
-            TagListView,
-            TagCreateView,
-            TagUpdateView,
-            TagDeleteView,
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    task_toggle_status_view,
+    TaskDeleteView,
 )
 
 urlpatterns = [
@@ -14,8 +17,11 @@ urlpatterns = [
     path("create_task/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path('task/<int:pk>/toggle-status/', task_toggle_status_view, name='task-toggle-status'),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Tag, Task
 
@@ -9,6 +9,9 @@ class TaskListView(ListView):
     model = Task
     template_name = "to_do/index.html"
     context_object_name = "task_list_all"
+
+    def get_queryset(self):
+        return Task.objects.order_by('done', 'deadline')
 
 
 class TagListView(ListView):
@@ -20,8 +23,20 @@ class TagListView(ListView):
 class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy("")
+    success_url = reverse_lazy("todo:index")
     template_name = "to_do/task_form.html"
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:index")
+    template_name = "to_do/task_form.html"
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+    template_name = "to_do/delete_confirm.html"
 
 
 class TagCreateView(CreateView):
@@ -41,4 +56,11 @@ class TagUpdateView(UpdateView):
 class TagDeleteView(DeleteView):
     model = Tag
     success_url = reverse_lazy("todo:tag-list")
-    template_name = "to_do/tag_delete_confirm.html"
+    template_name = "to_do/delete_confirm.html"
+
+def task_toggle_status_view(request, pk):
+    if request.method == 'POST':
+        task = get_object_or_404(Task, pk=pk)
+        task.done = not task.done
+        task.save()
+        return redirect("todo:index")
